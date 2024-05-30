27d6f4ee0ad3beeb4d56cc20a57a5e9c0a65b786
MirkoValle <mirko.valle20@gmail.com>
2024-05-30 16:19:15 +0200

    Added object array

--- js/script.js
+++ js/script.js
@@ -1,10 +1,33 @@
 const { createApp, ref } = Vue
 
 createApp({
-    setup() {
-
+    data() {
         return {
-
+            task: [
+                {
+                    text: "Finire l'esercizio",
+                    done: false,
+                },
+                {
+                    text: "Non disperarsi",
+                    done: false,
+                },
+                {
+                    text: "Dai su che ce la fai",
+                    done: false,
+                },
+                {
+                    text: "Non te ferma",
+                    done: false,
+                },
+                {
+                    text: "Dormire non è un opzione",
+                    done: false,
+                }
+            ],
         }
+    },
+    methods: {
+        
     }
 }).mount('#app')
